790fd15d212f0883e98ef3c951b0deae154e8fea
Jazyell94 <81425680+Jazyell94@users.noreply.github.com>
2025-07-15 00:30:57 -0300

    Update script.js

--- public/admin/script.js
+++ public/admin/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://delivery-system-production.up.railway.app';
+const API_BASE_URL = 'disciplined-determination-production.up.railway.app';
 
 let autoFetchEnabled = true;
 let fetchInterval;
